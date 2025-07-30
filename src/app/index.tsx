3c50328c83e339a77ff2aac903eed2ad24244cbe
Elias Joaquin Garcia <eliasgarcia81@gmail.com>
2025-07-30 12:46:03 -0300

    added errors and loader

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
 import TodoItem from "@/components/todoItem";
 import useTodos from "@/hooks/useTodos";
 import { TodoItemType } from "@/types";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 
 export default function Index() {
-  const { todos } = useTodos();
+  const { todos, error, loading } = useTodos();
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text style={{ marginTop: 10 }}>Loading todos...</Text>
+      </View>
+    );
+  }
 
   return (
     <View
@@ -15,12 +30,14 @@ export default function Index() {
       }}
     >
       <View>
-        {todos.length > 0 ? (
+        {error ? (
+          <Text style={{ color: "red", textAlign: "center" }}>{error}</Text>
+        ) : todos.length > 0 ? (
           todos.map((todo: TodoItemType) => {
             return <TodoItem key={todo.id} id={todo.id} todo={todo.todo} />;
           })
         ) : (
-          <Text>There is no tasks</Text>
+          <Text>There are no tasks</Text>
         )}
       </View>
     </View>
--- src/hooks/useTodos.tsx
+++ src/hooks/useTodos.tsx
@@ -1,22 +1,39 @@
+import { TodoApiResponse, TodoItemType } from "@/types";
 import { useEffect, useState } from "react";
-import { TodoItemType, TodoApiResponse } from "@/types";
 
 const useTodos = () => {
   const [todos, setTodos] = useState<TodoItemType[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
   const fetchTodos = async () => {
-    const data = await fetch(apiUrl as string);
-    const { todos } = (await data.json()) as TodoApiResponse;
-    setTodos(todos);
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch(apiUrl as string);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const { todos } = data as TodoApiResponse;
+      setTodos(todos);
+    } catch (error) {
+      setError(`Error fetching todos: ${error}`);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
-  return { todos };
+  return { todos, error, loading };
 };
 
 export default useTodos;
